2ab3da90f7f6fd30a18bc0a533035f0da4bcafd1
Artsem Stalavitski <a.stalavitski@gmail.com>
2022-04-18 10:06:32 +0200

    added users app

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+
+
+class Administrator(AbstractUser):
+    pass
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    created_by = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    iban = models.CharField(
+        max_length=32,
+        validators=[
+            # 11 chars for Norway BBAN (the smallest), 30 chars is max amount according to specification
+            RegexValidator(
+                regex='^[A-Z]{2}[0-9]{2}[A-Z0-9]{11,30}$',
+                message='IBAN is invalid. Value should consist only from alphanumeric characters',
+                code='invalid_iban'
+            ),
+        ]
+    )
--- www/settings.py
+++ www/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+AUTH_USER_MODEL = 'users.Administrator'
